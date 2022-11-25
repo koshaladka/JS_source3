1c0de312bc8cb57ff29be7b8808fdbd103cda03d
Alena Work <123@123.ru>
2022-11-25 09:53:50 +0300

    form

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,6 +1,13 @@
 export default class From {
     constructor(form) {
         this.forms = document.querySelectorAll(forms);
+        this.message = {
+            loading: 'Загрузка...',
+            success: 'Спасибо! Скоро мы с вами свяжемся',
+            failure: 'Что-то пошло не так...'  
+        };
+    
+        this.path = 'assets/question.php';
     }
 
     async postData(url,data) {
@@ -15,7 +22,28 @@ export default class From {
     init() {
         this.forms.forEach(item => {
             item.addEventListener('submit', (e) => {
-                e.preventDefault
+                e.preventDefault();
+
+                let statusMessage = document.createElement('div');
+                statusMessage.style.cssText =`
+                    margin-top: 15px;
+                    font-size: 18px;
+                    color: grey;
+                `;
+                item.parentNode.appendChild(statusMessage);
+
+                statusMessage.textContent = this.message.loading;
+
+                const formData = new FormData(item);
+
+                this.postData(this.path, formData)
+                    .then(res => {
+                        console.log(res);
+                        statusMessage.textContent = this.message.success;
+                    })
+                    .catch(() => {
+                        statusMessage.textContent = this.message.failure;
+                    })
             })
         })
     }
