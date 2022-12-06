1074442a815d9cbb79a43f2972edded8ed353591
Alena Work <123@123.ru>
2022-12-06 09:31:41 +0300

    slider page modules

--- src/js/modules/slider/slider-main.js
+++ src/js/modules/slider/slider-main.js
@@ -40,13 +40,7 @@ export default class MainSlider extends Slider {
         this.showSlides(this.slideIndex += n);
     }
 
-    render() {
-       try {
-        try {
-            this.hanson = document.querySelector('.hanson');
-        } catch(e) {}
-        
-
+    bindTriggers() {
         this.btns.forEach(item => {
             item.addEventListener('click', () => {
                 this.plusSlides(1);
@@ -56,11 +50,34 @@ export default class MainSlider extends Slider {
                 e.preventDefault();
                 this.slideIndex = 1;
                 this.showSlides(this.slideIndex);
-            })
+            });
         });
 
-        this.showSlides(this.slideIndex);
-       } catch(e) {}
+        document.querySelectorAll('.prevmodule').forEach(item => {
+            item.addEventListener('click', (e)=> {
+                e.stopPropagation();
+                e.preventDefault();
+                this.plusSlides(-1);
+            })
+        })
 
+        document.querySelectorAll('.nextmodule').forEach(item => {
+            item.addEventListener('click', (e)=> {
+                e.stopPropagation();
+                e.preventDefault();
+                this.plusSlides(1);
+            })
+        })
+    }
+
+    render() {
+       if (this.container) {
+        try {
+            this.hanson = document.querySelector('.hanson');
+        } catch(e) {}
+        
+        this.showSlides(this.slideIndex);
+        this.bindTriggers();      
+       } 
     }
 }
